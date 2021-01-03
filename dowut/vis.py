36da97db32da4e5e7a74fd8fad1463b7c7df234b
Christopher Sams <csams@redhat.com>
2021-01-03 11:06:20 -0600

    Minutes and percentage of activity.
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- dowut/vis.py
+++ dowut/vis.py
@@ -15,19 +15,49 @@ def load_df():
     return pd.concat(pd.read_csv(path, parse_dates=["time"]) for path in paths)
 
 
-def main():
-    df = load_df()
+def plot_focus_changes(df, ax):
+    df = df.copy()
+    df["delta"] = df.time.diff().apply(lambda d: d.total_seconds() / 60)
+    active = df[df.delta <= 10]
+
     grouper = pd.Grouper(key="time", freq="60min")
-    props = df[df.event_type == "PropertyNotify"].copy()
-    props["delta"] = props.time.diff().apply(lambda d: d.total_seconds() / 60)
-    sums = props.groupby([grouper, "wm_class"]).delta.sum().unstack()
+    sums = active.groupby([grouper, "wm_class"]).delta.sum().unstack()
 
     f = sums.fillna(0.0)
     f.index = [i.strftime("%a %H:%M") for i in f.index]
-    f = f.apply(lambda x: x/x.sum(), axis=1)
+    f = f.apply(lambda x: 100 * x / x.sum(), axis=1)  # normalize
 
-    fig, ax = plt.subplots(1)
-    ax = f.plot(kind="bar", stacked=True, ax=ax)
+    f.plot(kind="bar", stacked=True, ax=ax, title="Focused Window")
     ax.tick_params(axis="x", which="both", labelrotation=45)
+    ax.grid(axis="y")
+    ax.set_ylabel("percentage")
+
+
+def plot_active(df, ax):
+    df = df.copy()
+    df["delta"] = df.time.diff().apply(lambda d: d.total_seconds() / 60)
+    active = df[df.delta <= 10]
+
+    grouper = pd.Grouper(key="time", freq="60min")
+    sums = active.groupby([grouper, "wm_class"]).delta.sum().unstack()
+
+    f = sums.fillna(0.0)
+    f.index = [i.strftime("%a %H:%M") for i in f.index]
+
+    f.plot(kind="bar", ax=ax, stacked=True, title="Activity")
+    ax.tick_params(axis="x", which="both", labelrotation=45)
+    ax.grid(axis="y")
+    ax.set_ylabel("minutes")
+
+
+def main():
+    df = load_df()
+
+    fig, axes = plt.subplots(2)
+
+    plot_focus_changes(df, axes[0])
+    plot_active(df, axes[1])
+
     fig.set_tight_layout(True)
+
     plt.show()
